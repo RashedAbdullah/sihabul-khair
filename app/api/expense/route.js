8240c5a50be4fab2166e537a5fbd6f28ecf3df69
RashedAbdullah <maarashed40@gmail.com>
2024-11-12 12:41:54 +0600

    completed the get data for native app

---
+++ app/api/expense/route.js
@@ -0,0 +1,26 @@
+import { database_connection } from "@/database/database-connection";
+import { expenseModel } from "@/models/cost-model";
+import { NextResponse } from "next/server";
+
+export const GET = async (req) => {
+  try {
+    await database_connection();
+
+    const expenses = await expenseModel.find({});
+
+    return NextResponse.json({ success: true, data: expenses });
+  } catch (err) {
+    console.error("Error fetching expenses:", err);
+
+    const errorMessage = err.message || "An unexpected error occurred";
+    const statusCode = err.name === "MongoNetworkError" ? 503 : 500;
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: errorMessage,
+      },
+      { status: statusCode }
+    );
+  }
+};
---
+++ app/api/investment/route.js
@@ -0,0 +1,25 @@
+import { database_connection } from "@/database/database-connection";
+import { investmentModel } from "@/models/investment-model";
+import { NextResponse } from "next/server";
+
+export const GET = async (req) => {
+  try {
+    await database_connection();
+
+    const investments = await investmentModel.find({});
+    return NextResponse.json({ success: true, data: investments });
+  } catch (err) {
+    console.error("Error fetching investments:", err);
+
+    const errorMessage = err.message || "An unexpected error occurred";
+    const statusCode = err.name === "MongoNetworkError" ? 503 : 500;
+
+    return new NextResponse.json(
+      {
+        success: false,
+        message: errorMessage,
+      },
+      { status: statusCode }
+    );
+  }
+};
