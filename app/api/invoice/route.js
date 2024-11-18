570d2f78b0cf39462f702335e20caa820e3fbe88
RashedAbdullah <maarashed40@gmail.com>
2024-11-18 21:45:49 +0600

    Making the dynamic data fetching

--- app/api/invoice/route.js
+++ app/api/invoice/route.js
@@ -3,6 +3,8 @@ import { invoiceModel } from "@/models/invoice-model";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export const GET = async (req) => {
   try {
     await database_connection();
